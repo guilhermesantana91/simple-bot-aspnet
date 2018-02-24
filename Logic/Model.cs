ec6a491292a880a6f7f3c23d3b888649fc6f1428
logonrm <logonrm@fiap.com.br>
2018-02-24 16:30:33 -0300

    ygdsygdsygdsyg

---
+++ Logic/Model.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace SimpleBot.Logic
+{
+    public class Model : MongoClient
+    {
+        public Model() {
+
+            Model cliente = new Model();
+
+            var db = cliente.GetDatabase("Bot");
+            var col = db.GetCollection<BsonDocument>("teste");
+
+          
+    }
+
+
+        public void InserirDados()
+        {
+
+        }
+
+
+    }
+}
--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace SimpleBot
 {
@@ -9,7 +11,20 @@ namespace SimpleBot
     {
         public static string Reply(Message message)
         {
+            var cliente = new MongoClient();
+            var db = cliente.GetDatabase("bot");
+            var col = db.GetCollection<BsonDocument>("historico");
+
+            var doc = new BsonDocument
+            {
+                {"Texto",message.Text},
+                {"Usuario",message.User}
+            };
+
+            col.InsertOne(doc);
+
             return $"{message.User} disse '{message.Text}'";
+
         }
 
         public static UserProfile GetProfile(string id)
